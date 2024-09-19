7365166a11420a3f4e18631a4996ccd21da3efaf
Juan David Arguello Plata <juandavidarguello@gmail.com>
2024-09-19 08:17:08 -0500

    bug: solución de code smells criticos

--- auth/src/tests/integration/domain/type_permission_test.py
+++ auth/src/tests/integration/domain/type_permission_test.py
@@ -50,11 +50,11 @@ class TestTypePermission(unittest.TestCase):
 
         # Verifica que la instancia se haya guardado correctamente
         saved_type = db.session.get(Type, vp_type.typeId)
-        self.assertEqual(saved_type.name, 'VP Marketing')
-        self.assertEqual(saved_type.description, 'Vicepresidente de mercadeo')
-        self.assertEqual(saved_type.weight, 1000)
+        self.assertEqual(saved_type.name, vp_type.name)
+        self.assertEqual(saved_type.description, vp_type.description)
+        self.assertEqual(saved_type.weight, vp_type.weight)
         self.assertEqual(len(saved_type.permissions), 1)
-        self.assertEqual(saved_type.permissions[0].kind, 'Create')
+        self.assertEqual(saved_type.permissions[0].kind, self.create_permission.kind)
     
     def test_create_type_enroll_two_permissions(self):
         # Crea una instancia de Type y la almacena en la BD
@@ -66,33 +66,33 @@ class TestTypePermission(unittest.TestCase):
 
         # Verifica que la instancia se haya guardado correctamente
         saved_type = db.session.get(Type, presiedent.typeId)
-        self.assertEqual(saved_type.name, 'Presidente')
-        self.assertEqual(saved_type.description, 'Presidente corporativo')
-        self.assertEqual(saved_type.weight, 1100)
+        self.assertEqual(saved_type.name, presiedent.name)
+        self.assertEqual(saved_type.description, presiedent.description)
+        self.assertEqual(saved_type.weight, presiedent.weight)
         self.assertEqual(len(saved_type.permissions), 2)
 
     def test_create_two_rolls_one_permission(self):
         # Crea una instancia de Type y la almacena en la BD
-        presiedent = Type(name='Presidente', description='Presidente corporativo', weight=1100)
-        presiedent.permissions.append(self.create_permission)
-        self.db.session.add(presiedent)
+        vice = Type(name='Vice Presidente', description='Vice Presidente corporativo', weight=1000)
+        vice.permissions.append(self.create_permission)
+        self.db.session.add(vice)
         self.db.session.commit()
 
         # Crea otra instancia de Type y la almacena en la BD
-        vp_type = Type(name='VP Marketing', description='Vicepresidente de mercadeo', weight=1000)
-        vp_type.permissions.append(self.create_permission)
-        self.db.session.add(vp_type)
+        mensajero = Type(name='Mensajero', description='Mensajero')
+        mensajero.permissions.append(self.create_permission)
+        self.db.session.add(mensajero)
         self.db.session.commit()
 
         # Verifica que la instancia se haya guardado correctamente
         saved_types = db.session.query(Type).all()
         self.assertEqual(len(saved_types), 2)
-        self.assertEqual(saved_types[0].name, 'Presidente')
-        self.assertEqual(saved_types[1].name, 'VP Marketing')
+        self.assertEqual(saved_types[0].name, vice.name)
+        self.assertEqual(saved_types[1].name, mensajero.name)
         self.assertEqual(len(saved_types[0].permissions), 1)
         self.assertEqual(len(saved_types[1].permissions), 1)
-        self.assertEqual(saved_types[0].permissions[0].kind, 'Create')
-        self.assertEqual(saved_types[1].permissions[0].kind, 'Create')
+        self.assertEqual(saved_types[0].permissions[0].kind, self.create_permission.kind)
+        self.assertEqual(saved_types[1].permissions[0].kind, self.create_permission.kind)
     
 
 if __name__ == '__main__':
