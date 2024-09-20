82b08b8906b6ad60043ea98d4498c56a0195dc14
Juan David Arguello Plata <juandavidarguello@gmail.com>
2024-09-20 16:55:56 -0500

    bug: solución de code smells en role service

--- auth/src/tests/integration/application/role_service_test.py
+++ auth/src/tests/integration/application/role_service_test.py
@@ -7,8 +7,15 @@ class TestRoleService(TestConfigApplication):
         super().setUp()
         self.roleService = RoleService()
 
+        self.roles = [
+            {"name": "VP Colombia", "description": "Vice Presidento Corporativo - Filial Colombia", "typeId": self.types[0]["typeId"], "scopeId": self.scopes[0]["scopeId"]},
+            {"name": "Full-stack", "description": "Desarrollador full-stack", "typeId": self.types[1]["typeId"], "scopeId": self.scopes[0]["scopeId"]},
+            {"name": "VP Brasil", "description": "Vice Presidento Corporativo - Filial Brasil", "typeId": self.types[0]["typeId"], "scopeId": self.scopes[1]["scopeId"]},
+            {"name": "Full-stack BR", "description": "Desarrollador full-stack", "typeId": self.types[1]["typeId"], "scopeId": self.scopes[1]["scopeId"]}
+        ]
+
     def test_create_and_get_role_with_type_and_scope(self):
-        vp_colombia = self.roleService.create("VP Colombia", "Vice Presidento Corporativo - Filial Colombia", self.types[0]["typeId"], self.scopes[0]["scopeId"])
+        vp_colombia = self.roleService.create(**self.roles[0])
         vp_colombia_db = self.roleService.get_by_id(vp_colombia.roleId)
         self.assertEqual(vp_colombia_db.name, "VP Colombia")
         self.assertEqual(vp_colombia_db.description, "Vice Presidento Corporativo - Filial Colombia")
@@ -19,26 +26,28 @@ class TestRoleService(TestConfigApplication):
         self._not_found_role_error(-1, self.roleService.get_by_id)
     
     def test_get_role_by_scope(self):
-        vp_colombia = self.roleService.create("VP Colombia", "Vice Presidento Corporativo - Filial Colombia", self.types[0]["typeId"], self.scopes[0]["scopeId"])
-        vp_brasil = self.roleService.create("VP Brasil", "Vice Presidento Corporativo - Filial Brasil", self.types[0]["typeId"], self.scopes[1]["scopeId"])
+        vp_colombia = self.roleService.create(**self.roles[0])
+        vp_brasil = self.roleService.create(**self.roles[2])
         roles = self.roleService.get_by_scope(self.scopes[0]["scopeId"])
         self.assertEqual(len(roles), 1)
         self.assertEqual(roles[0].name, "VP Colombia")
 
-        full_stack_br = self.roleService.create("Full-stack BR", "Desarrollador full-stack", self.types[1]["typeId"], self.scopes[1]["scopeId"])
+        full_stack_br = self.roleService.create(**self.roles[3])
         roles = self.roleService.get_by_scope(self.scopes[1]["scopeId"])
         self.assertEqual(len(roles), 2)
         self.assertEqual(roles[0].name, "VP Brasil")
         self.assertEqual(roles[1].name, "Full-stack BR")
     
     def test_update_role(self):
-        vp_colombia = self.roleService.create("VP Brasil", "Vice Presidente Corporativo - Filial Brasil", self.types[0]["typeId"], self.scopes[0]["scopeId"])
+        self.roles[0]["name"] = "VP Brasil"
+        self.roles[0]["description"] = "Vice Presidente Corporativo - Filial Brasil"
+        vp_colombia = self.roleService.create(**self.roles[0])
         vp_colombia_updated = self.roleService.update(vp_colombia.roleId, new_name="VP Colombia", new_description="Vice Presidente Corporativo - Filial Colombia")
         self.assertEqual(vp_colombia_updated.name, "VP Colombia")
         self.assertEqual(vp_colombia_updated.description, "Vice Presidente Corporativo - Filial Colombia")
     
     def test_delete_role_that_exists(self):
-        full_stack_col = self.roleService.create("Full-stack", "Desarrollador full-stack", self.types[1]["typeId"], self.scopes[0]["scopeId"])
+        full_stack_col = self.roleService.create(**self.roles[1])
         self.assertIsNotNone(self.roleService.get_by_id(full_stack_col.roleId))
         self.roleService.delete(full_stack_col.roleId)
         self._not_found_role_error(full_stack_col.roleId, self.roleService.get_by_id)
